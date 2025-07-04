0126371dc4273cbfdb29e86e4398b9344c212486
AsnaaMcGurston <asnaamcgurston@gmail.com>
2025-07-04 15:18:07 +0700

    Update script.js

--- Portfolio-art-website/js/script.js
+++ Portfolio-art-website/js/script.js
@@ -10,21 +10,19 @@ const confirmation = document.getElementById("confirmation");
 
 form.addEventListener("submit", function(e){
     e.preventDefault();
+    console.log("Form berhasil")
 
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const size = document.getElementById("size").value;
-    const color = document.getElementById("input[name='color']:checked");
-    const note = document.getElementById("note").value
-})
+    const color = document.querySelector("input[name='color']:checked");
+    const note = document.getElementById("note").value;
 
-if (!name || !email || !size ||!color) {
+    if (!name || !email || !size ||!color) {
     confirmation.textContent = "Lengkapi dulu formulirnya! Take your time:)";
     confirmation.style.color = "red";
     return;
-}
-
-confirmation.textContent = 'Terimakasih sudah memesan, ${name}! Kami akan menghubungimu lewat email,';
-confirmation.style.color = "green"
-
-form.requestFullscreen();
+    }
+    confirmation.textContent = `Terimakasih sudah memesan, ${name}! Kami akan menghubungimu lewat email!`;
+    confirmation.style.color = "green"
+});
